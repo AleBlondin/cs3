f12473b4326b5c0d34d35a30ef721e4fe85de89a
AleBlondin <alexandreblondin@laposte.net>
2021-03-05 13:32:27 +0100

    perf-state

---
+++ perf/my-app/Cours.md
@@ -0,0 +1,15 @@
+# Cours centrale 2 - perf
+
+## Notion de state
+- notion de state
+- calcul inutile
+
+
+## Mémoization
+  - state
+  - callback
+
+# Fuite mémoire
+
+
+# Analyse de taille de bundle
--- perf/my-app/src/ProblematicComponent.js
+++ perf/my-app/src/ProblematicComponent.js
@@ -1,16 +1,19 @@
 import React from "react";
 
 const ProblematicComponent = () => {
+  let counter = 0;
+
   return (
     <>
       <div className="App">Some content</div>
 
       <button
         onClick={() => {
-          console.log("clicked");
+          counter += 1;
+          console.log({ counter });
         }}
       >
-        Click me
+        {counter}
       </button>
     </>
   );
