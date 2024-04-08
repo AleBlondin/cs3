2a2823cbbd155d38c900e1cebf183c3b603e1b8e
AleBlondin <alexandreblondin@laposte.net>
2024-04-08 13:50:31 +0200

    :sparkles: add broken example

--- test/src/sort.unit.test.js
+++ test/src/sort.unit.test.js
@@ -2,8 +2,8 @@ import { sort } from "./sort_naive";
 
 describe("sort", () => {
   it("should sort an array in ascending order", () => {
-    const unsortedArray = [15, 3, 2, 16, 18, 25, 22];
-    const expectedSortedArray = [2, 3, 15, 16, 18, 22, 25];
+    const unsortedArray = [4, 9, 3, 3, 5];
+    const expectedSortedArray = [3, 3, 4, 5, 9];
 
     expect(sort(unsortedArray)).toEqual(expectedSortedArray);
   });
--- test/src/sort_naive.js
+++ test/src/sort_naive.js
@@ -1,16 +1,5 @@
 export const sort = (list, compare) => {
-  const orderedList = [...list]; // Make a copy of the original list
+  const sortedNumbers = list.slice(0, list.length).sort();
 
-  for (let i = 0; i < orderedList.length - 1; i++) {
-    for (let j = 0; j < orderedList.length - i - 1; j++) {
-      // Swap elements if they are in the wrong order
-      if (orderedList[j] > orderedList[j + 1]) {
-        const temp = orderedList[j];
-        orderedList[j] = orderedList[j + 1];
-        orderedList[j + 1] = temp;
-      }
-    }
-  }
-
-  return orderedList;
+  return sortedNumbers;
 }
